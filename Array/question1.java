7fc0586bce15ac9d52572c8e31eaea64842dff60
Abhishek Jha <abhishekjha031@gmail.com>
2024-06-06 09:59:30 +0530

    commit

---
+++ Array/question1.java
@@ -0,0 +1,39 @@
+/*
+ Question 1:Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ Example 1:
+ Input:nums = [1, 2, 3, 1]
+ Output:   true 
+ 
+ Example 2:
+ Input:nums = [1, 2, 3, 4]
+ Output:   false 
+ Example 3:
+ Input:nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+ Output:   true 
+ 
+ Constraints:•1 <= nums .  
+ lengtth <= 105•
+ -109 <= nums [ i ] <= 109
+ */
+
+
+package Array;
+
+import java.util.Arrays;
+
+public class question1 {
+    public static boolean twise(int nums[]){
+        Arrays.sort(nums);
+        for(int i = 0 ; i<nums.length-1 ; i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+           
+        } return false;
+        
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,1};
+        System.out.println(twise(nums));
+    }
+}
